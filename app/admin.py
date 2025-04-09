baf6a6578fb7ffb43c6be594832d6aad15b8af95
shox404 <thesh8080@gmail.com>
2025-04-09 11:05:17 +0500

    Update

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TestBox, Test
 
-# Register your models here.
+admin.site.register(TestBox)
+
+admin.site.register(Test)
--- app/models.py
+++ app/models.py
@@ -1,15 +1,35 @@
 from django.db import models
 
+
 class TestBox(models.Model):
     title = models.CharField(max_length=70)
 
     def __str__(self):
-        return f"{self.title}"
+        return self.title
 
 
 class Test(models.Model):
-    question = models.CharField(max_length=100)
-    test_box = models.ForeignKey(TestBox, on_delete=models.CASCADE, related_name='tests')
+    ANSWER_CHOICES = [
+        ("FI", "First Choice"),
+        ("SE", "Second Choice"),
+        ("TH", "Third Choice"),
+        ("FO", "Fourth Choice"),
+    ]
+
+    test_box = models.ForeignKey(
+        TestBox, on_delete=models.CASCADE, related_name="tests"
+    )
+    question = models.CharField(max_length=255)
+
+    first = models.CharField(max_length=100)
+    second = models.CharField(max_length=100)
+    third = models.CharField(max_length=100)
+    fourth = models.CharField(max_length=100)
+
+    correct_answer = models.CharField(max_length=2, choices=ANSWER_CHOICES)
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return self.question
+
+    def get_correct_text(self):
+        return self[self.correct_answer]
